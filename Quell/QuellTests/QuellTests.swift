cbfab766778596ef2be0629dc209dfb19f68fd79
Bailey Wallace <wallacebailey32@gmail.com>
2026-04-30 18:31:29 -0400

    Add Xcode project for Quell with proper gitignore

---
+++ Quell/QuellTests/QuellTests.swift
@@ -0,0 +1,18 @@
+//
+//  QuellTests.swift
+//  QuellTests
+//
+//  Created by Bailey Wallace on 4/30/26.
+//
+
+import Testing
+
+struct QuellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
